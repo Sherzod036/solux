be7796a55c7158124f138d18a3c8908eaf6faff7
sherzod <sherzod.erkinov036@gmail.com>
2021-10-09 20:00:51 +0500

    half-layouts

--- js/app.js
+++ js/app.js
@@ -1,18 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const generateLayouts = []
+	// const generateLayouts = []
 
-	for (let i = 0; i < 5; i++) {
-		generateLayouts.push(`layout_${i + 1}`)
-	}
+	// for (let i = 0; i < 5; i++) {
+	// 	generateLayouts.push(`layout_${i + 1}`)
+	// }
+
+	// const changeBodyBg = (id, addClass, tween) => {
+	// 	new ScrollMagic.Scene({ triggerElement: id })
+	// 		.setTween(tween)
+	// 		.setClassToggle(document.body, addClass)
+	// 		.addTo(controller)
+	// }
+
+	// generateLayouts.map(i => changeBodyBg(`#${i}`, i, tween))
 
 	const controller = new ScrollMagic.Controller()
+	// Tweens
+	const tween = TweenMax.to('.hero__graphic', 2, { rotation: -43 })
 
-	const changeBodyBg = (id, addClass) => {
-		new ScrollMagic.Scene({ triggerElement: id })
-			.setClassToggle(document.body, addClass)
+		new ScrollMagic.Scene({ triggerElement: '#layout_2' })
+			.setTween(tween)
+			.setClassToggle(document.body, 'layout_2')
 			.addTo(controller)
-	}
 
-	generateLayouts.map(i => changeBodyBg(`#${i}`, i))
+		new ScrollMagic.Scene({ triggerElement: '#layout_3' })
+			.setClassToggle(document.body, 'layout_3')
+			.addTo(controller)
+
+		new ScrollMagic.Scene({ triggerElement: '#layout_4' })
+			.setClassToggle(document.body, 'layout_4')
+			.addTo(controller)
+
+		new ScrollMagic.Scene({ triggerElement: '#layout_5' })
+			.setClassToggle(document.body, 'layout_5')
+			.addTo(controller)
+
+
+
 
 })
